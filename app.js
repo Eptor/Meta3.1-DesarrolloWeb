091c094d3e888dc098d4e0e6809ee92ad0ce6dcd
Hector Espinoza <hector_espinoza02@hotmail.com>
2024-04-25 01:55:12 -0700

    Certificados autofirmados, CORS

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
+const cors = require('cors');
 const app = express();
 app.use(express.json());
 
+// Configuración de CORS
+const corsOptions = {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+// Rutas de tu aplicación
 const activosRouter = require('./router/activos');
 const ubicacionesRouter = require('./router/ubicaciones');
 const responsablesRouter = require('./router/responsables');
-
 app.use('/activos', activosRouter);
 app.use('/ubicaciones', ubicacionesRouter);
 app.use('/responsables', responsablesRouter);
 
 const port = 3000;
-app.listen(port, () => {
-    console.log(`Servidor escuchando en http://localhost:${port}`);
+
+const options = {
+    key: fs.readFileSync('certs/private.key'),
+    cert: fs.readFileSync('certs/certificate.crt')
+};
+
+https.createServer(options, app).listen(port, () => {
+    console.log(`Servidor escuchando en https://localhost:${port}`);
 });
